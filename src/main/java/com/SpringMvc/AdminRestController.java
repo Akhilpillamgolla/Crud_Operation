f39f623c76bdc088f86d40acb91ee681598198dc
Akhilpillamgolla <84654724+Akhilpillamgolla@users.noreply.github.com>
2024-04-03 11:44:48 +0530

    Add files via upload

---
+++ src/main/java/com/SpringMvc/AdminRestController.java
@@ -0,0 +1,39 @@
+package com.SpringMvc;
+
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PatchMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/admin") // Optional
+public class AdminRestController {
+	
+	@GetMapping("/show")
+	public String helloMsgGet() {
+		return "Hello From GET";
+	}
+
+	@PostMapping("/show")
+	public String helloMsgPost() {
+		return "Hello From POST";
+	}
+
+	@PutMapping("/show")
+	public String helloMsgPut() {
+		return "Hello From PUT";
+	}
+
+	@DeleteMapping("/show")
+	public String helloMsgDelete() {
+		return "Hello From DELETE";
+	}
+
+	@PatchMapping("/show")
+	public String helloMsgPatch() {
+		return "Hello From PATCH";
+	}
+}
